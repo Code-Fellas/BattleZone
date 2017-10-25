d2a1e1bf0990ac98cc47cda3453a266b9d3bd799
sirjan13 <sirjan13@yahoo.com>
2017-10-25 20:32:46 +0530

    New App User/

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.core.validators import validate_email
+
+# Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255, default=None)
+    last_name = models.CharField(max_length=255, default=None)
+    email = models.CharField(max_length=255, validators=[validate_email])
+    mobile_number = models.charField(max_length=11)
+    password = models.CharField(max_length=255, default=None)
+    sex = models.CharField()
+    current_year = models.IntegerField(default=0)
+    current_university = models.CharField()
+    is_active = models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
